bc19b24046c743cbcc81c56e131b5979e61747d1
iyunq <111261315+iyunq@users.noreply.github.com>
2024-05-17 12:10:12 +0700

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-107c2c1f-45ee-4c11-8637-18ab38a70452/"
 
 OUTPUT_DIR="logs"
 
